114d2ea1220bce6818d287a8e612f590a603c6fd
viet_v <vohoangviet459@gmail.com>
2024-09-13 23:24:04 +0700

    Replace broken API with new one, refactor functions to fetch and display quote correctly because new API contains only one quote on each call

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class QuoteMachine extends Component {
   // This method fetches a new quote
   fetchQuote = () => {
     // const CORS_PROXY = 'https://proxy.cors.sh/';
-    const API_URL = 'https://api.quotable.io/quotes/random?limit=30';
+    const API_URL = 'https://quotes-api-self.vercel.app/quote';
 
     /* 
     fetch(`${CORS_PROXY}${API_URL}`, {
@@ -40,34 +40,40 @@ class QuoteMachine extends Component {
       .then(response => response.json())
       .then(data => {
         // Store all quotes in state
-        this.setState({quotes: data}, () => {
+        this.setState({
+          quotes: data,
+          currentQuote: data.quote,
+          currentAuthor: data.author
+        }, () => {
           // This callback runs after the state has been updated
           // Set the first quote to be default
-          if (data.length > 0) {
-            this.setQuote(0)
-          }
+          console.log(this.state.quotes)
+          // if (data.length > 0) {
+          //   this.setQuote(0)
+          // }
+          
         });
       })
       .catch(error => console.error('Error fetching quote:', error));
   }
 
+  // FOR API THAT CONTAINS ARRAY OF QUOTES
   // Set current quote and author
-  setQuote = (index) => {
+  /* setQuote = (index) => {
     const selectedQuote = this.state.quotes[index];
     if (selectedQuote) { // Ensure selectedQuote is not undefined
       this.setState({
-        currentQuote: selectedQuote.content,
+        currentQuote: selectedQuote.quote,
         currentAuthor: selectedQuote.author
       });
     } else {
       console.error('Selected quote is undefined');
     }
   }
-
   randomQuote = () => {
     const index = Math.floor(Math.random() * this.state.quotes.length);
     this.setQuote(index);
-  }
+  } */
   
   // componentDidMount runs when this component is mounted to DOM
   componentDidMount() {
@@ -75,21 +81,21 @@ class QuoteMachine extends Component {
   }
 
 
-render() {
-  return (
-    <div className="App">
-      <div className="wrapper" id="quote-box">
-        <img src={logo} alt="React" className="App-logo"/>
-        <p id="text">{this.state.currentQuote}</p>
-        <p id="author">- {this.state.currentAuthor}</p>
-        <div className="button-container">
-          <a href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="' + this.state.currentQuote + '" - ' + this.state.currentAuthor} target="_blank" id="tweet-quote" rel="noreferrer">Tweet</a>
-          <button id="new-quote" onClick={this.randomQuote}>New Quote</button>
+  render() {
+    return (
+      <div className="App">
+        <div className="wrapper" id="quote-box">
+          <img src={logo} alt="React" className="App-logo"/>
+          <p id="text">{this.state.currentQuote}</p>
+          <p id="author">- {this.state.currentAuthor}</p>
+          <div className="button-container">
+            <a href={'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="' + this.state.currentQuote + '" - ' + this.state.currentAuthor} target="_blank" id="tweet-quote" rel="noreferrer">Tweet</a>
+            <button id="new-quote" onClick={this.fetchQuote}>New Quote</button>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default QuoteMachine;
